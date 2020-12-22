bb77c02da3567ea308014dd8d5207e6e3f907624
YunHyeRim1 <0305_08@naver.com>
2020-12-22 18:21:37 +0900

    18

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,32 +4,59 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@SessionAttributes({"ctx","cmm","mgr","tea","uss"})
 public class HomeController {
+	@Autowired HttpSession session;
+	@Autowired HttpServletRequest request;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
-		logger.info("======== Welcome home! =========");
-		
-		
-		
-		return "index";
-	}
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/")
+    public String index(HttpSession session, HttpServletRequest request) {
+    	String ctx = request.getContextPath();
+    	session.setAttribute("ctx", ctx);
+    	session.setAttribute("cmm", ctx+"/resources/cmm");
+    	session.setAttribute("mgr", ctx+"/resources/mgr");
+    	session.setAttribute("tea", ctx+"/resources/tea");
+    	session.setAttribute("uss", ctx+"/resources/uss");
+    	
+        logger.info("Project Initialized ... ");
+        return "index";
+    }
+    
+    @GetMapping("/move/{dir}/{page}")
+    public String move(@PathVariable String dir, @PathVariable String page) {
+        logger.info("이동 디렉토리: " + dir);
+        logger.info("이동 파일: " + page);
+        return String.format("%s/%s", dir, page);
+    }
+
+    @GetMapping("/transfer/{dir}/{sub}/{page}")
+    public String transfer(@PathVariable String dir, @PathVariable String sub, @PathVariable String page) {
+        logger.info("이동 디렉토리: " + dir);
+        logger.info("이동 서브 디렉토리: " + sub);
+        logger.info("이동 파일: " + page);
+        return String.format("%s/%s/%s", dir, sub, page);
+    }
 	
 }
---
+++ src/main/webapp/resources/cmm/js/index.js
@@ -0,0 +1,14 @@
+/**
+var map = new HashMap<>();
+var cmm = cmm || {}
+ */
+var cmm = cmm || {}
+cmm = (() => {
+	const init = ctx => {
+		alert(`실행@@@ ${ctx}`)
+		$('.move-home').click(e => {location.href = '/'})
+		$('#manager_register').click(e => { location.href = `${ctx}/transfer/sym/mgr/register`})
+		$('#teacher_register').click(e => { location.href = `${ctx}/transfer/sym/tea/register`})
+	}
+	return {init}
+})()
