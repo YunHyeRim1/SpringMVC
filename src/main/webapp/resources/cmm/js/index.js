ffd520538c01086eb3944af87a91f2ea5baa8bb1
YunHyeRim1 <0305_08@naver.com>
2020-12-23 15:38:14 +0900

    15

--- src/main/webapp/resources/cmm/js/index.js
+++ src/main/webapp/resources/cmm/js/index.js
@@ -3,12 +3,17 @@ var map = new HashMap<>();
 var cmm = cmm || {}
  */
 var cmm = cmm || {}
-cmm = (() => {
+cmm = (()=>{
 	const init = ctx => {
-		alert(`실행@@@ ${ctx}`)
+		alert(`>>`)
 		$('.move-home').click(e => {location.href = '/'})
 		$('#manager_register').click(e => { location.href = `${ctx}/transfer/sym/mgr/register`})
 		$('#teacher_register').click(e => { location.href = `${ctx}/transfer/sym/tea/register`})
+		$(`#linkedin`).click(e => {
+			location.href = `${ctx}/home`
+		})
+		
 	}
+	
 	return {init}
 })()
